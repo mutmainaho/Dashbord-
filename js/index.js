b94ea788c2c9f276ecc1decd6fbeb94fa30454d2
Sadiq Aderinto <sadiqaderinto@Sadiq-3.local>
2020-12-15 08:46:12 +0100

    added js and redesigned the website

---
+++ js/index.js
@@ -0,0 +1,14 @@
+console.log('hello')
+
+table = document.querySelector("tbody")
+
+add_button = document.getElementById('add-icon-svg')
+
+const addRow = () => {
+    rows = table.querySelectorAll('tr')
+    lastRow = rows[rows.lenght -1]
+
+    table.appen
+}
+
+
